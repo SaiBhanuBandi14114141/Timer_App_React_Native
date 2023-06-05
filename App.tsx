1ab43c329a10f6db9c576eff0c854c6a3c3232d3
SaiBhanuBandi14114141 <105918956+SaiBhanuBandi14114141@users.noreply.github.com>
2023-06-05 07:26:57 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-// import StopWatch from './screens/stopWatch';
 import Timer from './screens/timer';
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      {/* <StopWatch /> */}
       <Timer />
     </SafeAreaView>
   );
